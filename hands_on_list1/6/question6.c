0f89f302249f1b8991d3463e3b7c6f5cf70d08cb
Vicky Panchal <vmp9905@gmail.com>
2023-08-23 22:13:58 +0530

    completed till que 9

--- hands_on_list1/6/question6.c
+++ hands_on_list1/6/question6.c
@@ -2,8 +2,19 @@
 #include<unistd.h>
 
 int main() {
-	char s[1024];
+    char buffer[1024];
 
-	scanf("%s",s);
-	printf("%s",s);
+    size_t bytesRead;
+    while ((bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer))) > 1) {
+        
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    if (bytesRead == -1) {
+        perror("Error reading from STDIN");
+        return 1;
+    }
+
+    return 0;
+
 }
